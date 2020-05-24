5a8798d719a35457921e4a2b1dd7115ec5d6f676
larell mcbride <mcbridelarell@gmail.com>
2020-05-24 19:44:34 -0400

    Final commit

---
+++ StudentDatabaseApp.java
@@ -0,0 +1,26 @@
+package StudentDatabase;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+
+        // Ask how many new students we want to add
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
+        // Create n number of new students
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+            // System.out.println(stu1.showInfo());
+            // same as below
+            System.out.println(students[n].toString());
+        }
+        System.out.println(students[0]);
+        System.out.println(students[1]);
+    }
+}
